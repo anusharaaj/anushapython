ef5e658629d7d7ecc1c29db996debf0ed8c41564
anusharaaj <nakkalaanusha26@gmail.com>
2021-02-02 15:26:36 +0530

    third commit

---
+++ anusha04.py
@@ -0,0 +1,9 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from getpass import getpass
+uname=input('enter u name')
+pwd=getpass('enter pwd')
+if uname=="sai" and pwd=="sai@123":
+    print('Welcome',uname)
+else:
+    print('invalid uname or password')
